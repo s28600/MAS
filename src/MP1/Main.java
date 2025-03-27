f2e7c07d41938b8e6fe5cb793647b60e42e823d9
s28600 <s28600@pjwstk.edu.pl>
2025-03-27 17:38:02 +0100

    MP1 wip 2

--- src/MP1/Main.java
+++ src/MP1/Main.java
@@ -1,10 +1,11 @@
 package MP1;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 class Cat {
-
+    static List<Cat> cats = new ArrayList<Cat>();
     static int min_month_to_sterilize = 5;
 
     private String name;
@@ -13,38 +14,99 @@ class Cat {
     private LocalDate birthdate;
 
     public Cat(String name, List<String> vaccines_done, LocalDate birthdate) {
-        this.name = name;
-        this.vaccines_done = vaccines_done;
-        this.birthdate = birthdate;
+        setName(name);
+        setVaccines_done(vaccines_done);
+        setBirthdate(birthdate);
+        cats.add(this);
     }
 
     public Cat(String name, String race, List<String> vaccines_done, LocalDate birthdate) {
-        this.name = name;
-        this.race = race;
-        this.vaccines_done = vaccines_done;
-        this.birthdate = birthdate;
+        this(name, vaccines_done, birthdate);
+        setRace(race);
+    }
+
+    public static int getMin_month_to_sterilize() {
+        return min_month_to_sterilize;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getRace() {
+        return race;
+    }
+
+    public List<String> getVaccines_done() {
+        return vaccines_done;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public static void setMin_month_to_sterilize(int min_month_to_sterilize) {
+        if (min_month_to_sterilize <= 0) {
+            throw new IllegalArgumentException("Minimum month to sterilize cannot be zero or negative");
+        }
+        Cat.min_month_to_sterilize = min_month_to_sterilize;
+    }
+
     public void setName(String name) {
-        if (name.isEmpty()){
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        } else if (name.isEmpty()){
             throw new IllegalArgumentException("Name cannot be empty");
         }
         this.name = name;
     }
 
-    public String getRace() {
-        return race;
-    }
-
     public void setRace(String race) {
-        if (race.isEmpty()){
+        if (race == null) {
+            throw new IllegalArgumentException("Race cannot be null");
+        } else if (race.isEmpty()){
             throw new IllegalArgumentException("Race cannot be empty");
         }
         this.race = race;
+    }
+
+    public void setVaccines_done(List<String> vaccines_done) {
+        if (vaccines_done == null) {
+            throw new IllegalArgumentException("Vaccines done cannot be null");
+        } else if (vaccines_done.isEmpty()) {
+            throw new IllegalArgumentException("Vaccines done cannot be empty");
+        }
+        this.vaccines_done = vaccines_done;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        if (birthdate == null) {
+            throw new IllegalArgumentException("Birthdate cannot be null");
+        } else if (birthdate.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("Birthdate cannot be in future");
+        }
+        this.birthdate = birthdate;
+    }
+
+    public String getAge(){
+        int months = LocalDate.now().minusMonths(birthdate.getMonthValue()).getMonthValue();
+        if (months/12 > 0) return months/12 + " years, " + (months%12) + "months";
+        return months + " months";
+    }
+
+    public static String getByRace(String race){
+        if (race == null) {
+            throw new IllegalArgumentException("Race cannot be null");
+        } else if (race.isEmpty()){
+            throw new IllegalArgumentException("Race cannot be empty");
+        }
+
+        List<String> filtered = new ArrayList<>();
+        for (Cat cat : cats) {
+             if (cat.getRace().equals(race)) filtered.add(cat.toString());
+        }
+
+        return filtered.toString();
     }
 
     @Override
